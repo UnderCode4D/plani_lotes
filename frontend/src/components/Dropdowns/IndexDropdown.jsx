6c465a81b8f28d037c6fe2e124dfad639a47f600
Frnn4268 <appstutorialesy@gmail.com>
2024-11-24 12:56:48 -0600

    Aañadiendo nuevo Login.jsx y Register.jsx y sus rutas

--- frontend/src/components/Dropdowns/IndexDropdown.jsx
+++ frontend/src/components/Dropdowns/IndexDropdown.jsx
@@ -56,6 +56,26 @@ const IndexDropdown = () => {
           Crear Cuenta
         </Link>
         <div className="h-0 mx-4 my-2 border border-solid border-blueGray-100" />
+        <span
+          className={
+            "text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-blueGray-400"
+          }
+        >
+          Layout de Autenticación - Cliente
+        </span>
+        <Link
+          to="/auth/login"
+          className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+        >
+          Iniciar Sesión
+        </Link>
+        <Link
+          to="/auth/register"
+          className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+        >
+          Crear Cuenta
+        </Link>
+        <div className="h-0 mx-4 my-2 border border-solid border-blueGray-100" />
         <Link
           to="/landing"
           className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
