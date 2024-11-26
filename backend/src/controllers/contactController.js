0404391842904b2d3079461cd441912124c3ef7b
Frnn4268 <appstutorialesy@gmail.com>
2024-11-26 12:09:29 -0600

    Añadiendo nueva respuesta de contacto utilizando NodeMailer y su util nodeMailer.js

--- backend/src/controllers/contactController.js
+++ backend/src/controllers/contactController.js
@@ -1,10 +1,30 @@
 const Contact = require('../models/contactModel');
 const createError = require('../utils/appError');
+const sendEmail = require('../utils/nodeMailer');
 
 // Create a new contact
 exports.createContact = async (req, res, next) => {
   try {
     const newContact = await Contact.create(req.body);
+
+    const currentDate = new Date().toLocaleString();
+
+    await sendEmail({
+      email: newContact.email,
+      subject: 'Confirmación de Contacto',
+      html: `
+         <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
+            <p>Hola ${newContact.full_name},</p>
+            <p>Gracias por contactarnos en UnderCode. Hemos recibido tu mensaje el ${currentDate} y nuestro equipo se pondrá en contacto contigo lo antes posible.</p>
+            <p>Aquí está una copia de tu mensaje:</p>
+            <blockquote>${newContact.message}</blockquote>
+            <p>Si tienes alguna pregunta adicional o necesitas más información, no dudes en responder a este correo.</p>
+            <p>Puedes contactarnos directamente en: <a href="mailto:undercode4d@gmail.com">undercode4d@gmail.com</a></p>
+            <p>Saludos cordiales,<br>El equipo de UnderCode</p>
+            <img src="cid:logo" alt="UnderCode Logo" style="width: 100px; height: auto; margin-top: 20px;">
+         </div>
+        `,
+    });
 
     res.status(201).json({
       status: 'success',
